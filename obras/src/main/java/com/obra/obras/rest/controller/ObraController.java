47d2f765c6549600322595c04bed76fd21a0bb68
José Milton de Oliveira Neto <milton_oliveira18@live.com>
2022-11-07 17:04:13 -0300

    Att ObraControlle

--- obras/src/main/java/com/obra/obras/rest/controller/ObraController.java
+++ obras/src/main/java/com/obra/obras/rest/controller/ObraController.java
@@ -5,12 +5,10 @@ import com.obra.obras.domain.repository.Obras;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/obras")
